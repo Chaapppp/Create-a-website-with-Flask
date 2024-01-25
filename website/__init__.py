5286ad35440198ecd9d29d75fa65f1fb4758c918
Keetasin Kongsee <keetasin01596@gmail.com>
2024-01-25 11:10:29 +0700

    views_auth

--- website/__init__.py
+++ website/__init__.py
@@ -4,8 +4,15 @@ from flask import Flask
 def create_app():
     # Create an instance of the Flask class
     app = Flask(__name__)
-    
     # Set a secret key for the application (used for session management)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+
+    from .views import views
+    from .auth import auth
+
+    # Register the blueprints with the main application
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
 
     return app
