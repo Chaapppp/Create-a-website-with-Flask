0a7eb73f51993d912c087994d761fbd5729e07dc
Keetasin Kongsee <keetasin01596@gmail.com>
2024-01-29 12:19:35 +0700

    welcome.html

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint ,render_template
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def welcome():
-    return '<p>welcome</p>'
+    return render_template('welcome.html')
