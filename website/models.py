73416ea826482476890d05d8d02b58b77e733739
Keetasin Kongsee <keetasin01596@gmail.com>
2024-01-31 22:24:19 +0700

    bmi records

--- website/models.py
+++ website/models.py
@@ -1,6 +1,15 @@
 from . import db
 from flask_login import UserMixin
 
+class BmiRecords(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    weight = db.Column(db.Float)
+    height = db.Column(db.Float)
+    bmi = db.Column(db.Float)
+    categories = db.Column(db.String(50))
+    now = db.Column(db.String(50))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
