cd5ab7046e147de869ac401d20a2d0464ce1905f
Keetasin Kongsee <keetasin01596@gmail.com>
2024-02-02 08:52:40 +0700

    logout

--- website/auth.py
+++ website/auth.py
@@ -16,7 +16,7 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.welcome'))
+                return redirect(url_for('views.bmi'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -24,8 +24,10 @@ def login():
     return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required  
 def logout():
-    return '<p>logout</p>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
