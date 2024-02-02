8abce3df1f221edd33146bceca9d0a3a25ea25c3
Keetasin Kongsee <keetasin01596@gmail.com>
2024-02-02 08:47:11 +0700

    current_user

--- website/views.py
+++ website/views.py
@@ -1,39 +1,39 @@
 from flask import Blueprint ,render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def welcome():
-    return render_template('welcome.html')
+    return render_template('welcome.html', user=current_user)
 
 @views.route('/account')
 def account():
-    return render_template('account.html')
+    return render_template('account.html', user=current_user)
 
 @views.route('/article')
 def article():
-    return render_template('article.html')
+    return render_template('article.html', user=current_user)
 
 @views.route('/Categories')
 def Categories():
-    return render_template('Categories.html')
+    return render_template('Categories.html', user=current_user)
 
 @views.route('/normal')
 def normal():
-    return render_template('normal.html')
+    return render_template('normal.html', user=current_user)
 
 @views.route('/obese')
 def obese():
-    return render_template('obese.html')
+    return render_template('obese.html', user=current_user)
 
 @views.route('/overweight')
 def overweight():
-    return render_template('overweight.html')
+    return render_template('overweight.html', user=current_user)
 
 @views.route('/underweight')
 def underweight():
-    return render_template('underweight.html')
+    return render_template('underweight.html', user=current_user)
 
 
 
@@ -46,4 +46,4 @@ def underweight():
 @views.route('/bmi')
 @login_required
 def bmi():
-    return render_template('bmi.html')
+    return render_template('bmi.html', user=current_user)
